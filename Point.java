cd5eddc71ecfb631bc5fc45a333cb8493f068655
sisse008 <sisse008@umn.edu>
2018-04-30 11:45:27 +0300

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,31 @@
+
+
+public class Point
+{
+	private double x;
+	private double y;
+	private double z;
+
+	public Point(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public double X()
+	{
+		return x;
+	} 
+
+	public double Y()
+	{
+		return y;
+	}
+
+	public double Z()
+	{
+		return z;
+	}
+
+};
